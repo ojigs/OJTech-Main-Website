3d90c1645f0a661b9384256bb41fc820178fecb0
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-09-05 04:29:25 +0100

    dark mode toggler implemented

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const collapsibleButton = document.querySelectorAll('.collapsible')
 const navbarToggler = document.querySelector('.navbar-toggler')
 const navbarClose = document.querySelector('.navbar-close')
 const navbarCollapse = document.querySelector('.navbar-collapse')
+const source = document.querySelectorAll('source')
 
 
 for (let i=0; i < collapsibleButton.length; i++) {
@@ -21,14 +22,23 @@ navbarToggler.addEventListener('click', function() {
     navbarCollapse.classList.toggle('navbar-active')
     this.style.display = 'none'
     navbarClose.style.display = 'block'
-
-    console.log('click happened')
 })
 
 navbarClose.addEventListener('click', function() {
     navbarCollapse.classList.remove('navbar-active')
     this.style.display = 'none'
     navbarToggler.style.display = 'block'
-
-    console.log('click happened')
 })
+
+//Dark mode toggler: changes partners image source
+  document.addEventListener('colorschemechange', (e) => {
+    console.log(`Color scheme changed to ${e.detail.colorScheme}.`);
+    source.forEach(src => {
+        if (e.detail.colorScheme == 'dark') {
+            src.setAttribute('media', '(prefers-color-scheme: dark)') 
+        } else {
+            src.setAttribute('media', '(prefers-color-scheme: light)')
+        }
+        
+    })
+  });
